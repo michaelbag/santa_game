9384aa242f542c6474fc46739a15ce907c83f28e
Michael BAG <mk@p7net.ru>
2025-12-16 03:10:08 +0300

    Добавлена титульная страница с переходом на админку Django

--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.conf import settings
 
-# Create your views here.
+
+def index(request):
+    """Главная страница с информацией о проекте и ссылкой на админку"""
+    context = {
+        'project_name': 'Игра "Тайный Санта"',
+        'version': '1.1',
+        'author': 'Michael BAG',
+        'author_telegram': '@michaelbag',
+        'author_email': 'mk@remark.pro',
+        'admin_url': '/admin/',
+    }
+    return render(request, 'bot/index.html', context)
